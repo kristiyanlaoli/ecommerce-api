a81dde46439382e447a3b31389152eae86f66017
Kristiyan <kristiyanlaoli@gmail.com>
2024-03-14 23:17:06 +0700

    fix information again

--- index.js
+++ index.js
@@ -8,7 +8,7 @@ app.use(express.json());
 
 app.get("/api", (req, res) => {
   res.json({
-    message: "Welcome to our E-commerce API",
+    message: "Welcome to simple E-commerce API",
     status: "Under Development",
     purpose: "Learning to create an API",
     author: "Kristiyan Laoli",
