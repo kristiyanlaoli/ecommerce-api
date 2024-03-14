e1fc8f8765d3097afa923ad4db5b7801280d114b
Kristiyan <kristiyanlaoli@gmail.com>
2024-03-14 12:18:07 +0700

    try deploy to vercel again

--- api/index.js
+++ api/index.js
@@ -1,7 +1,10 @@
 import express from "express";
-import router from "./app/router.js";
-
+import router from "../app/router.js";
+import dotenv from "dotenv";
+dotenv.config();
 const app = express();
+app.use(express.json());
+
 app.use((req, res, next) => {
   res.setHeader("Access-Control-Allow-Origin", "http://localhost:5173"); // Ganti dengan URL aplikasi React Anda
   res.setHeader(
@@ -13,16 +16,20 @@ app.use((req, res, next) => {
   next();
 });
 
-app.use(express.json());
-app.use(router);
-
 app.get("/api", (req, res) => {
   res.json({ message: "Hello, welcome to ecommerce-api" });
 });
+app.use(router);
 
 app.use((err, req, res, next) => {
   console.error(err.stack);
   res.status(500).json({ message: "Something broke!" });
+});
+
+const port = process.env.APP_PORT || 3200;
+
+app.listen(port, () => {
+  console.log(`Listening on port ${port}`);
 });
 
 export default app;
